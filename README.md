9a50eacb61ed222cd76dd9d8a41e7ee8c8f34f58
Luis Lázaro <lalazaro@keedio.com>
2015-08-18 14:42:09 +0200

    INT-32 initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Scala-Sonar
+===========================
+
+This is a simple programa in Scala language to test Scoverage Plugin for Sonar.
---
+++ src/test/scala/org/keedio/sonar/test/SimpleApp.scala
@@ -0,0 +1,17 @@
+package org.keedio.sonar.test
+
+/**
+ * Created by luislazaro on 18/8/15.
+ * lalazaro@keedio.com
+ * Keedio
+ */
+object SimpleApp {
+
+    def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+
+    def main(args : Array[String]) {
+        println( "Hello World!" )
+        println("concat words = " + foo(Array("uno","dos","tres")))
+    }
+
+}
